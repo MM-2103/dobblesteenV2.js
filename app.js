b837444b72c204f3ee74016ab7a11497479b0a59
MM-2103 <mohsen.menem1@gmail.com>
2023-04-06 01:28:13 +0200

    updated index.html

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const dice = document.getElementById('cube');
-const button = document.getElementById('roll');
+// const dice = document.getElementById('cube');
+// const button = document.getElementById('roll');
 
 
-let min = 1;
-let max = 6;
+// let min = 1;
+// let max = 6;
 
-button.onclick = function rollTheDice() {
-    var xRand = getRandom(max, min);
-    var yRand = getRandom(max, min);
+// button.onclick = function rollTheDice() {
+//     var xRand = getRandom(max, min);
+//     var yRand = getRandom(max, min);
 
-    dice.style.webkitTransform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
-    dice.style.transform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
-}
+//     dice.style.webkitTransform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
+//     dice.style.transform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
+// }
 
-function getRandom(max, min) {
-    return (Math.floor(Math.random() * (max-min)) + min) * 90;
-  }
+// function getRandom(max, min) {
+//     return (Math.floor(Math.random() * (max-min)) + min) * 90;
+//   }
+
+

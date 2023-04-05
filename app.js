24a9f18577429b5a8040c6af1a68b82230b38f2a
MM-2103 <mohsen.menem1@gmail.com>
2023-04-05 13:21:03 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const dice = document.getElementById('cube');
+const button = document.getElementById('roll');
+
+
+let min = 1;
+let max = 6;
+
+button.onclick = function() {
+    var xRand = getRandom(max, min);
+    var yRand = getRandom(max, min);
+
+    dice.style.webkitTransform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
+    dice.style.transform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
+}
+
+function getRandom(max, min) {
+    return (Math.floor(Math.random() * (max-min)) + min) * 90;
+  }
